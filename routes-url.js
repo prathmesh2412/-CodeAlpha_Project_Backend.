b2a70be8d104470558ad79fe6c6c14f3c32646ff
prathmesh2412 <khanapureprathmesh086@gmail.com>
2025-03-02 14:20:39 +0530

    Delete routes-url.js

--- routes-url.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const{ handleGenerateNewShortURL,  handlegetAnalytics } = require('../controllers/Controller-url.js');
-const router = express.Router();
-
-router.post("/", handleGenerateNewShortURL);
-
-router.get('/analytics/:shortId',handlegetAnalytics)
-module.exports = router;
